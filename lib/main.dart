618e65908056b5db63b653375774c028f666c95b
aprimr <aprimregmi24@gmail.com>
2026-01-14 15:40:35 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Text("data"),
+    );
+  }
+}
